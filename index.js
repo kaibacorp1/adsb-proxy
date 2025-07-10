51d1107f1ed76529153d8836f0e0c20d580cc2d7
kaibacorp1 <sandu.godakumbura@gmail.com>
2025-07-10 23:49:27 +1200

    Update index.js

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ app.get('/planes', async (req, res) => {
   }
 });
 
-const port = process.env.PORT || 8080;
+const port = process.env.PORT || 10000;
 app.listen(port, () => {
   console.log(`Server listening on port ${port}`);
 });
